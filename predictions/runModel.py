ad2a2dc8b5b9726b97185034f3bfe7e2e80a8fb0
Chris <mendozac@udel.edu>
2021-11-30 16:09:04 -0500

    quick python script update to show on test

--- predictions/runModel.py
+++ predictions/runModel.py
@@ -1,6 +1,6 @@
 import os
 
-os.system("java -cp \"./weka.jar\" weka.classifiers.bayes.BayesNet -T single_instance.arff -l current_model.model -p 0 -c 2 > prediction.txt")
+os.system("java -cp \"./weka.jar\" weka.classifiers.bayes.BayesNet -T test_set.arff -l current_model.model -p 0 -c 2 > prediction.txt")
 # open the sample file used
 file = open('prediction.txt')
   
@@ -10,8 +10,25 @@ content = file.readlines()
 #read prediction of single instance
 val = content[5][30]
 
+val2 = content[41][34]
+
+numErrors = 0
+numInstances = len(content) - 6
+for i in range(5,len(content) - 1):
+    val3 = content[i][34]
+    if val3 == "+":
+        numErrors += 1
+
+print(numErrors)
+print("number of errors: ") 
+print(numErrors)
+print("number of instances: ")
+print(numInstances)
+print("accuracy: ")
+print((numInstances - numErrors)/(numInstances))
+
 #print prediction
-if(int(val) == 0):
-    print("NOT MALICIOUS")
-else:
-    print("MALICIOUS")
+#if(int(val) == 0):
+   # print("NOT MALICIOUS")
+#else:
+   # print("MALICIOUS")
