41e80668c95ca0e9ad59a554af7db3bcb5bd24b7
chrisnmendoza <mendozac@udel.edu>
2021-12-02 18:41:00 -0500

    attempting to resolve file conversion issues

--- detecting/runBayesNet.py
+++ detecting/runBayesNet.py
@@ -20,6 +20,35 @@ if(dataFile[-3:-1] + dataFile[-1] == "csv"):
     os.system("java -cp \"./weka.jar\" weka.core.converters.CSVLoader " + dataFile + " > " + arffFile + " -B 1000")
     dataFile = arffFile
 
+    #open arff
+    arff = open(dataFile)
+    arffContent = arff.readlines()
+
+    #generate a new file
+    generated = open("generated.arff", 'a')
+
+    #read from header
+    headerFile = open("headers.txt")
+    headerContent = headerFile.readlines()
+
+    #write header content to generated
+    generated.writelines(headerContent)
+    generated.writelines("\n")
+
+    #write arff content to generated
+    generated.writelines(arffContent[4097:])
+    print("WHY PARSE" + arffContent[4097])
+    generated.close()
+    generatedWriteable = open("generated.arff")
+    generatedContent = generatedWriteable.readlines()
+
+    #convert arff file to generated
+    arffWriteable = open("flamingo.txt", "w")
+    arffWriteable.writelines(generatedWriteable)
+
+    #remove generated
+    #os.remove("generated.arff")
+
 #run model on generated .arff
 os.system("java -cp \"./weka.jar\" weka.classifiers.bayes.BayesNet -T " + dataFile + " -l bayesNet.model -p 0 -c 2 > prediction.txt")
 
@@ -30,12 +59,24 @@ file = open('prediction.txt')
 content = file.readlines()
 
 #read prediction of single instance
-dataPoint = content[5]
+val = content[5][30]
 
-if "benign" in dataPoint:
-    print("it is not malware")
-elif "malicious" in dataPoint:
-    print("malicious!")
+val2 = content[41][34]
+
+numErrors = 0
+numInstances = len(content) - 6
+for i in range(5,len(content) - 1):
+    val3 = content[i][34]
+    if val3 == "+":
+        numErrors += 1
+
+print(numErrors)
+print("number of errors: ") 
+print(numErrors)
+print("number of instances: ")
+print(numInstances)
+print("accuracy: ")
+print((numInstances - numErrors)/(numInstances))
 
 #print prediction
 #if(int(val) == 0):
