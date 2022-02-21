8e8d594247a23a9671647ceb2cfb57648d1e0144
aepickett <alex@ultimatetournament.io>
2022-02-21 15:47:23 -0500

    fixed git

--- MalwareEngine/engineV1/node_modules/node-virustotal/queryBuilderTester.js
+++
@@ -1,3 +0,0 @@
-var QB = require("./queryBuilder.js");
-console.log(QB.sizeAtMost(45));
-var intelAPI = require("intelligenceAPI.js").makeIapiConnection();
--- Testing/node_modules/node-virustotal/cliLauncher.js
+++
@@ -1,3 +0,0 @@
-#! /usr/bin/env node
-"use strict";
-require(__dirname + "/cli.js");
